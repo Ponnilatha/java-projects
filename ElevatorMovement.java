61e399e2f6341a906063b16b0b85835441091cbf
Ponnilatha <ponnilatha@gmail.com>
2016-02-23 17:16:36 -0500

    Added files via upload

---
+++ ElevatorMovement.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.latha.ess.cons;
+
+/**
+ * @author latha
+ *
+ */
+public enum ElevatorMovement {
+UP,DOWN,HOLD;
+}
---
+++ ElevatorStatus.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.latha.ess.cons;
+
+/**
+ * @author anwita
+ *
+ */
+public enum ElevatorStatus {
+	OCCUPIED,EMPTY;
+
+}
