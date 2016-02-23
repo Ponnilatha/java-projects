18be0a20c2d86b1bc6591f6359530aa65eeaca9a
Ponnilatha <ponnilatha@gmail.com>
2016-02-23 17:55:42 -0500

    Added files via upload

---
+++ InvalidNoOfElevator.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.latha.ess.exception;
+
+/**
+ * @author latha
+ *
+ */
+public class InvalidNoOfElevator extends Exception {
+
+	public InvalidNoOfElevator(String message) {
+		super(message);
+	}
+	
+}
