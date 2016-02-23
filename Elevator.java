6d7c46f07e514f8bd718b19a707c51ce5bd4e68d
Ponnilatha <ponnilatha@gmail.com>
2016-02-23 17:13:09 -0500

    Added files via upload

---
+++ Elevator.java
@@ -0,0 +1,59 @@
+package org.latha.ess;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+import org.latha.ess.cons.ElevatorMovement;
+import org.latha.ess.cons.ElevatorStatus;
+
+public class Elevator { private Integer currentFloor;
+private Queue<Integer> destinationFloors;
+
+public Elevator(Integer currentFloor) {
+  this.currentFloor = currentFloor;
+  this.destinationFloors = new PriorityQueue<Integer>();
+}
+
+public int nextDestionation(){
+  return this.destinationFloors.peek();
+}
+
+public int currentFloor(){
+  return this.currentFloor;
+}
+
+public void popDestination(){
+  this.destinationFloors.remove();
+}
+
+public void addNewDestinatoin(Integer destination) {
+  this.destinationFloors.add(destination);
+}
+
+	
+public void moveUp() {
+  currentFloor++;
+}
+
+
+public void moveDown() {
+  currentFloor--;
+}
+
+
+public ElevatorMovement direction() {
+  if (destinationFloors.size() > 0){
+    if (currentFloor < destinationFloors.peek()){
+      return ElevatorMovement.UP;
+    } else if (currentFloor > destinationFloors.peek()) {
+      return ElevatorMovement.UP;
+    }
+  }
+  return ElevatorMovement.HOLD;
+}
+
+public ElevatorStatus status() {
+  return (destinationFloors.size() > 0)?ElevatorStatus.OCCUPIED:ElevatorStatus.EMPTY;
+}
+
+}
