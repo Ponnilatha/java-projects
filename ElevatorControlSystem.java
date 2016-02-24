7ae1284756b2d579fdc159c636936658ccc7f7ff
Ponnilatha <ponnilatha@gmail.com>
2016-02-23 19:04:52 -0500

    Update ElevatorControlSystem.java

--- ElevatorControlSystem.java
+++ ElevatorControlSystem.java
@@ -4,7 +4,9 @@
 package org.latha.ess;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -22,12 +24,14 @@ public class ElevatorControlSystem {
 	Integer numberOfElevators = 0;
 	Integer numberOfFloors = 0;
 	List<Elevator> elevators;
+	Map<Integer,Integer> noOfTripsByElevator;
 	Queue<Integer> pickupfloors;
 	
 	public ElevatorControlSystem(Integer numberOfElevators, Integer numberOfFloors) throws InvalidNoOfElevator{
 		 if (numberOfElevators < 0) throw new InvalidNoOfElevator("Elevator numbers must be atleast 1 to 10");
 		 this.numberOfElevators = (numberOfElevators > MAXNO_OF_ELEVATORS)?MAXNO_OF_ELEVATORS:numberOfElevators;
 		 this.numberOfFloors = numberOfFloors;
+		 noOfTripsByElevator = new HashMap<Integer,Integer>();
 		 initializeElevators();
 		 pickupfloors = new PriorityQueue<Integer>();
 		 
@@ -54,10 +58,31 @@ public class ElevatorControlSystem {
 	    elevators.get(elevatorId).addNewDestinatoin(destinationFloor);
 	  }
 
+	  private void tripCount(Elevator elevator) {
+		  int counter = elevator.getNoOfTripsByElevator().intValue();
+		  noOfTripsByElevator.put(elevator.getElevatorId(), counter++);
+	  }
 	  
+	  private boolean checkMaxTrips(Elevator elevator)  {
+		  boolean isElevatorUsed = true;
+		 if( noOfTripsByElevator.get(elevator.getElevatorId()) > 100){
+			 elevators.remove(elevator.getElevatorId());
+			 System.out.println("Elevator Under Maintence");
+			 isElevatorUsed = false;
+		 }
+		 return isElevatorUsed;
+	  }
+
 	  public void step() {
 	    // Loop though every elevator
 	    for (Elevator currElevator : elevators){
+	    	
+	    	// checking for max trips and remove the elevator from list and continue
+	    	if(!checkMaxTrips(currElevator)){
+	    		continue;
+	    	}
+	    	tripCount(currElevator);
+	   
 	      // Check to figure out which ones are unoccupied and update call
 	      switch (currElevator.status()){
 	        case EMPTY:
@@ -77,9 +102,7 @@ public class ElevatorControlSystem {
 	              currElevator.moveDown();
 	              break;
 	            case HOLD:
-	              // TODO: Check timer here to alert users that they are holding the door open to long
-	              // TODO: Emergency situation where elevator can't be used
-	              // TODO: Maintenance Mode e.g. movers or maintenance people
+	              
 	              currElevator.popDestination();
 	              break;
 	          }
